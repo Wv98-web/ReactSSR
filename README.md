5e4d7c38474416f9bd72f2bc9d869359bc150748
wuwei <wuwei@hidream.net>
2022-02-09 09:22:24 +0800

    first commit --构建简单React服务器渲染

---
+++ README.md
@@ -0,0 +1,4 @@
+### 主要技术 React express webpack
+
+### 打包 npm run build
+### 项目启动 npm run start
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+import Home from "./containers/Home/index";
+import React from "react";
+import { renderToString } from "react-dom/server";
+
+// 虚拟Dom是真实DOM的一个javascript对象映射 提升页面渲染性能
+// 服务器渲染ssr 首屏速度加快，seo效果提升
+
+// 客户端渲染
+// React代码在浏览器上执行，消耗的是用户浏览器的性能
+
+// 服务器端渲染
+// React代码在服务器上执行，消耗的是服务器端的性能
+
+const app = express();
+const port = 3000;
+const content = renderToString(<Home />);
+
+app.get("/", (req, res) => {
+	res.send(`
+    <html>
+      <head>
+        <title>ReactSSR</title>
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port http://localhost:${port}`);
+});
